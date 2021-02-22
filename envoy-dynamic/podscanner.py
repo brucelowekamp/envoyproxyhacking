77d77420163eda4293aecc6e05cacb239e0399a4
Bruce Lowekamp <bruce.lowekamp@skype.net>
2021-02-22 10:55:16 -0500

    update config in a loop

--- envoy-dynamic/podscanner.py
+++ envoy-dynamic/podscanner.py
@@ -1,7 +1,26 @@
-from kubernetes import client, config
+from io import StringIO
+import kubernetes
+import os
+import tempfile
+from time import sleep
+import yaml
 
-name = "helloworld"
-port = 5000
+CONFIG_FILE = "podscanner.yaml"
+
+cds_filename = None
+lds_filename = None
+name = None
+port = None
+poll_s = None
+
+with open (CONFIG_FILE) as f:
+  config = yaml.load(f, Loader=yaml.BaseLoader)
+  cds_filename = config['cdsfile']
+  lds_filename = config['ldsfile']
+  poll_s = int(config['poll_seconds'])
+  name = config['services'][0]['name']
+  port = config['services'][0]['port']
+
 
 CDS_INTRO = """resources:
 - "@type": type.googleapis.com/envoy.config.cluster.v3.Cluster
@@ -74,27 +93,47 @@ LDS_DEFAULT = """              - match:
                 route:
                   cluster: {name}"""
 
-config.load_incluster_config()
+kubernetes.config.load_incluster_config()
+v1 = kubernetes.client.CoreV1Api()
 
+lastcds = None
+lastlds = None
 
-v1 = client.CoreV1Api()
-ret = v1.list_pod_for_all_namespaces(watch=False)
+while True:
+  # print ("scan")
+  scan = v1.list_pod_for_all_namespaces(watch=False)
+  cds = StringIO()
+  lds = StringIO()
+  print (CDS_INTRO.format(name=name, port=port), file=cds)
+  print (LDS_INTRO.format(name=name, port=port), file=lds)
+  for i in scan.items:
+    #print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+    if i.metadata.name.startswith(name):
+      # str.removeprefix() requires py3.9 :(
+      id = i.metadata.name.replace(name+"-", "")
+      ip = i.status.pod_ip
+      if ip is None:
+        # pod still initializing
+        continue
+      #print("%s\t%s" % (n, ip))
+      print (CDS_HOST.format(name=name, port=port, id=id, ip=ip), file=cds)
+      print (LDS_HOST.format(name=name, port=port, id=id, ip=ip), file=lds)
+  print (LDS_DEFAULT.format(name=name, port=port), file=lds)
 
-with open ("cds.yaml", "w") as cds:
-  with open ("lds.yaml", "w") as lds:
-    print (CDS_INTRO.format(name=name, port=port), file=cds)
-    print (LDS_INTRO.format(name=name, port=port), file=lds)
-    for i in ret.items:
-      #print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
-      if i.metadata.name.startswith(name):
-        # str.removeprefix() requires py3.9 :(
-        id = i.metadata.name.replace(name+"-", "")
-        ip = i.status.pod_ip
-        if ip is None:
-          # pod still initializing
-          continue
-        #print("%s\t%s" % (n, ip))
-        print (CDS_HOST.format(name=name, port=port, id=id, ip=ip), file=cds)
-        print (LDS_HOST.format(name=name, port=port, id=id, ip=ip), file=lds)
-    print (LDS_DEFAULT.format(name=name, port=port), file=lds)
+  newcds = cds.getvalue()
+  newlds = lds.getvalue()
+
+  if newcds != lastcds or newlds != lastlds:
+    print("update clusters")
+    with tempfile.NamedTemporaryFile(mode='w', prefix='cds', delete=False) as tempcds:
+      with tempfile.NamedTemporaryFile(mode='w', prefix='lds', delete=False) as templds:
+        print (newcds, file=tempcds)
+        print (newlds, file=templds)
+        tempcds.close()
+        templds.close()
+        os.rename(tempcds.name, cds_filename)
+        os.rename(templds.name, lds_filename)
+        lastcds = newcds
+        lastlds = newlds
+  sleep(poll_s)
 
